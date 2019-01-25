bcff96faf4c53cd00df4b45465d6410aa54c6c67
happyAnger6 <happyAnger6@163.com>
2019-01-25 12:30:25 +0000

    1-25

--- blogan/models.py
+++ blogan/models.py
@@ -1,7 +1,13 @@
+import datetime
+
 from blogan import db
 
-class Category(db.Document):
-    pass
+class User(db.Document):
+   name = db.StringField(required=True)
+   email = db.StringField()
+   type = db.IntField()
+   gold = db.IntField()
+   register_time = db.DateTimeField(default=datetime.datetime.utcnow)
 
 class Category(db.Document):
     name = db.StringField(required=True)
@@ -10,6 +16,26 @@ class Category(db.Document):
     url = db.StringField()
     level = db.IntField()
     children = db.ListField(db.StringField())
-    flag = db.IntField()
+    showFlag = db.IntField()
+
+class Comment(db.Document):
+    author = db.StringField()
+    content = db.StringField()
+    publish_data = db.DateTimeField(default=datetime.datetime.utcnow)
+    showFlag = db.IntField(default=1)
+    comments = db.ListField(db.StringField())
+
+class Post(db.Document):
+    title = db.StringField(required=True)
+    content = db.StringField()
+    category = db.StringField()
+    chaper = db.IntField()
+    section = db.IntField()
+    author = db.StringField()
+    publish_data = db.DateTimeField(default=datetime.datetime.utcnow)
+    tags = db.ListField(db.StringField())
+    comments = db.ListField(db.EmbeddedDocumentField(Comment))
+    type = db.IntField()
+    showFlag = db.IntField()
 
 model_lst = [Category]
