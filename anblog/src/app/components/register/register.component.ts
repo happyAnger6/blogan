aca8fee92af1fa833361b0538936ae2bf91e504d
happyAnger6 <happyAnger6>
2019-02-09 10:02:51 +0000

    2-9

--- anblog/src/app/components/register/register.component.ts
+++ anblog/src/app/components/register/register.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
+import { User } from '../../models/user';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  register_form: FormGroup;
+  user: User = new User();
+  passwd_a: string = '';
   constructor() { }
 
   ngOnInit() {
+    this.register_form = new FormGroup({
+      'name': new FormControl(this.user.realname,[
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'passwd': new FormControl(this.user.password,[
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      'passwd_again': new FormControl(this.passwd_a,[
+        Validators.required,
+        Validators.minLength(6)
+      ])
+      });
   }
 
+  get name() { return this.register_form.get('name'); }
+  get passwd() { return this.register_form.get('passwd'); }
+  get passwd_again() { return this.register_form.get('passwd_again'); }
+
+  onSubmit() {
+    console.log(this.name);
+  }
 }
--- anblog/src/app/nav-top/nav-top.component.ts
+++ anblog/src/app/nav-top/nav-top.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-top',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-top.component.css']
 })
 export class NavTopComponent implements OnInit {
-
+  searchForm = new FormGroup({
+    searchTitle: new FormControl(''),
+    }
+  );
   constructor() { }
 
   ngOnInit() {
